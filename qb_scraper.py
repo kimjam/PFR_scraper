276da1999cf3441b27edf1ff771a6f3bfcc4bb2e
kimjam <kimjam@users.noreply.github.com>
2015-08-17 12:11:30 -0400

    linting

--- qb_scraper.py
+++ qb_scraper.py
@@ -4,79 +4,90 @@ import re
 import numpy as np
 import pandas as pd
 import itertools
-import os
-import pymysql
 import sqlalchemy
 
+
 def qb_scraper(qb_dict, target_date):
-	def PlayerScrape(name, playerlink):
-		url = playerlink
-		r = requests.get(url)
-		soup = BeautifulSoup(r.content)
-		data=[]
-		table = soup.find(class_ = 'sortable stats_table row_summable')
-		#table = soup.find('table', {'id': 'stats'}) #class_ = 'sortable stats_table row_summable')
-		for row in table.find_all('tr')[1:]:
-			col = row.find_all('td')
-			if len(col) > 0:
-				for c in col:
-					data.append(c.text)
-		header = soup.find_all("tr", {"class":''})[1]
-		regex = r'<th .*>(.*)<\/th>'
-		clmheaders = [re.compile(regex).findall(str(h)) for h in header]
-		chain = itertools.chain.from_iterable(clmheaders)
-		clmheaders = list(chain)
-		clmheaders[5] = 'Home'
-		
-		cols = len(clmheaders)
-		rows = len(data)/cols
-		
-		vals = pd.DataFrame(index = [name]*(rows-1), columns=clmheaders[0:22])
-		breaks = np.linspace(0,len(data), (rows+1), dtype="int16")
-		
-		for i in range(rows-1):
-			vals.ix[i] = [d for d in data[breaks[i]:breaks[i+1]]][0:22]
+    def PlayerScrape(name, playerlink):
+        url = playerlink
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content)
+        data = []
+        table = soup.find(class_='sortable stats_table row_summable')
+        for row in table.find_all('tr')[1:]:
+            col = row.find_all('td')
+            if len(col) > 0:
+                for c in col:
+                    data.append(c.text)
 
-		vals.index.name = 'Name'
-		vals.reset_index(level = 0, inplace = True)
-		return vals
+        header = soup.find_all("tr", {"class": ''})[1]
+        regex = r'<th .*>(.*)<\/th>'
+        clmheaders = [re.compile(regex).findall(str(h)) for h in header]
+        chain = itertools.chain.from_iterable(clmheaders)
+        clmheaders = list(chain)
+        clmheaders[5] = 'Home'
 
-	clmheaders = [
-		'Name', 'Rk', 'G#', 'Date', 'Age', 'Tm', 'Home', 'Opp',
-		'Result', 'GS', 'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'Int', 'Rate', 'Y/A',
-		'AY/A', 'Att', 'Yds', 'Y/A', 'TD', 'Tgt', 'Rec', 'Yds', 'Y/R', 'TD'
-	]
+        cols = len(clmheaders)
+        rows = len(data)/cols
 
-	QBS = pd.DataFrame(columns = clmheaders[0:23])
-	for key in qb_dict:
-		print key
-		p = PlayerScrape(key, qb_dict[key])
-		if p.shape[1] == QBS.shape[1]:
-			QBS = QBS.append(p)
-		else:
-			continue
-		print key
+        vals = pd.DataFrame(index=[name]*(rows-1), columns=clmheaders[0:22])
+        breaks = np.linspace(0, len(data), (rows+1), dtype="int16")
 
-	target_date = pd.to_datetime(target_date)
-	#QBS.index.name = 'name'
-	QBS = QBS.fillna(0)
-	QBS['Home'] = QBS['Home'].replace('@', 0).replace('', 1)
-	QBS['GS'] = QBS['GS'].replace('*', 1).replace('', 0)
-	QBS['Date'] = QBS['Date'].apply(pd.to_datetime)
-	QBS = QBS.replace('', 0)
-	QBS = QBS.drop(['Rk', 'Age', 'Result', 'Cmp%', 'Rate', 'Y/A', 'AY/A'], axis = 1)
-	QBS.columns = ['name', 'g', 'date', 'team', 'home', 'opp', 'gs', 'cmp',
-		'att', 'yds', 'td', 'ints', 'ru_att', 'ru_yds', 'ru_td']
-	QBS[['cmp', 'att', 'yds', 'td', 'ints', 'ru_att', 'ru_yds', 'ru_td']] = QBS[['cmp', 'att', 'yds', 'td', 'ints', 'ru_att', 'ru_yds', 'ru_td']].astype(int)
-	QBS['pts'] = QBS['yds'] / 25 + QBS['ru_yds'] / 10 + QBS['td'] * 4 - QBS['ints'] + QBS['ru_td'] * 6
-	QBS['pts'] = [round(x, 1) for x in QBS['pts']]
+        for i in range(rows-1):
+            vals.ix[i] = [d for d in data[breaks[i]:breaks[i+1]]][0:22]
 
-	QBS = QBS[QBS.date >= target_date]
-	QBS['date'] = QBS['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
-	
-	f = open('secret.txt', 'r')
-	secret = f.read()
+        vals.index.name = 'Name'
+        vals.reset_index(level=0, inplace=True)
+        return vals
 
-	connect_string = 'mysql+pymysql://root:%s@127.0.0.1/nfl?charset=utf8mb4' % (secret)
-	engine = sqlalchemy.create_engine(connect_string, echo = False)
-	QBS.to_sql(con = engine, name = 'qb', if_exists = 'append', index = False)
+    clmheaders = [
+        'Name', 'Rk', 'G#', 'Date', 'Age', 'Tm', 'Home', 'Opp',
+        'Result', 'GS', 'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'Int', 'Rate',
+        'Y/A', 'AY/A', 'Att', 'Yds', 'Y/A', 'TD', 'Tgt', 'Rec', 'Yds', 'Y/R',
+        'TD'
+    ]
+
+    QBS = pd.DataFrame(columns=clmheaders[0:23])
+    for key in qb_dict:
+        print key
+        p = PlayerScrape(key, qb_dict[key])
+        if p.shape[1] == QBS.shape[1]:
+            QBS = QBS.append(p)
+        else:
+            continue
+        print key
+
+    target_date = pd.to_datetime(target_date)
+    QBS = QBS.fillna(0)
+    QBS['Home'] = QBS['Home'].replace('@', 0).replace('', 1)
+    QBS['GS'] = QBS['GS'].replace('*', 1).replace('', 0)
+    QBS['Date'] = QBS['Date'].apply(pd.to_datetime)
+    QBS = QBS.replace('', 0)
+    QBS = QBS.drop(
+        ['Rk', 'Age', 'Result', 'Cmp%', 'Rate', 'Y/A', 'AY/A'],
+        axis=1
+    )
+    QBS.columns = [
+        'name', 'g', 'date', 'team', 'home', 'opp', 'gs', 'cmp',
+        'att', 'yds', 'td', 'ints', 'ru_att', 'ru_yds', 'ru_td'
+    ]
+    QBS[
+        ['cmp', 'att', 'yds', 'td', 'ints', 'ru_att', 'ru_yds', 'ru_td']
+    ] = QBS[
+        ['cmp', 'att', 'yds', 'td', 'ints', 'ru_att', 'ru_yds', 'ru_td']
+    ].astype(int)
+    QBS['pts'] = QBS['yds'] / 25 + QBS['ru_yds'] / (
+        10 + QBS['td'] * 4 - QBS['ints'] + QBS['ru_td'] * 6)
+
+    QBS['pts'] = [round(x, 1) for x in QBS['pts']]
+
+    QBS = QBS[QBS.date >= target_date]
+    QBS['date'] = QBS['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+
+    f = open('secret.txt', 'r')
+    secret = f.read()
+
+    connect_string = 'mysql+pymysql://root:%s@127.0.0.1/nfl?charset=utf8mb4'
+    connect_string = connect_string % (secret)
+    engine = sqlalchemy.create_engine(connect_string, echo=False)
+    QBS.to_sql(con=engine, name='qb', if_exists='append', index=False)
--- rb_scraper.py
+++ rb_scraper.py
@@ -4,78 +4,84 @@ import re
 import numpy as np
 import pandas as pd
 import itertools
-import os
-import pymysql
 import sqlalchemy
 
+
 def rb_scraper(rb_dict, target_date):
-	def PlayerScrape(name, playerlink, attributes):
-		url = playerlink
-		r = requests.get(url)
-		soup = BeautifulSoup(r.content)
-		data=[]
-		table = soup.find(class_ = 'sortable stats_table row_summable')
-		for row in table.find_all('tr')[1:]:
-			col = row.find_all('td')
-			if len(col) > 0:
-				for c in col:
-					data.append(c.text)
-		header = soup.find_all("tr", {"class":''})[1]
-		regex = r'<th .*>(.*)<\/th>'
-		clmheaders = [re.compile(regex).findall(str(h)) for h in header]
-		chain = itertools.chain.from_iterable(clmheaders)
-		clmheaders = list(chain)
-		clmheaders[5] = 'Home'
-		
-		
-		cols = len(clmheaders)
-		rows = len(data)/cols
-		
-		vals = pd.DataFrame(columns=clmheaders[0:17])
-		breaks = np.linspace(0,len(data), (rows+1), dtype="int16")
-		
-		for i in range(rows-1):
-			vals.loc[i] = [d for d in data[breaks[i]:breaks[i+1]]][0:17]
+    def PlayerScrape(name, playerlink, attributes):
+        url = playerlink
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content)
+        data = []
+        table = soup.find(class_='sortable stats_table row_summable')
+        for row in table.find_all('tr')[1:]:
+            col = row.find_all('td')
+            if len(col) > 0:
+                for c in col:
+                    data.append(c.text)
 
-		vals.insert(0, 'Name', name)
-		if len(vals.columns) != 18 or sum(vals.columns == attributes) != 18:
-			null = pd.DataFrame(columns = attributes)
-			return null
-		else:
-			return vals
+        header = soup.find_all("tr", {"class": ''})[1]
+        regex = r'<th .*>(.*)<\/th>'
+        clmheaders = [re.compile(regex).findall(str(h)) for h in header]
+        chain = itertools.chain.from_iterable(clmheaders)
+        clmheaders = list(chain)
+        clmheaders[5] = 'Home'
 
-	attributes = (['Name', 'Rk', 'G#', 'Date', 'Age', 'Tm', 'Home', 
-		'Opp', 'Result', 'Att', 'Yds', 'Y/A', 'TD', 'Tgt', 'Rec', 'Yds', 
-		'Y/R', 'TD'])
-	RBS = pd.DataFrame(columns = attributes)
+        cols = len(clmheaders)
+        rows = len(data)/cols
 
-	for key in rb_dict:
-		print key
-		p = PlayerScrape(key, rb_dict[key], attributes)
-		if sum(p.columns == RBS.columns) == 18:
-			RBS = RBS.append(p)
-			print key
+        vals = pd.DataFrame(columns=clmheaders[0:17])
+        breaks = np.linspace(0, len(data), (rows+1), dtype="int16")
 
-	target_date = pd.to_datetime(target_date)
-	RBS = RBS.fillna(0)
-	RBS['Home'] = RBS['Home'].replace('@', 0).replace('', 1)
-	RBS['Date'] = RBS['Date'].apply(pd.to_datetime)
-	RBS = RBS.drop(['Rk', 'Age', 'Result', 'Y/A', 'Y/R'], axis = 1)
-	RBS = RBS.replace('', 0)
-	RBS.columns = ['name', 'g', 'date', 'team', 'home', 'opp', 'att', 
-		'ru_yds', 'ru_td', 'tgt', 'rec', 'rec_yds', 'rec_td']
+        for i in range(rows-1):
+            vals.loc[i] = [d for d in data[breaks[i]:breaks[i+1]]][0:17]
 
-	RBS[['ru_yds', 'ru_td', 'rec', 'rec_yds', 'rec_td']] = RBS[['ru_yds', 'ru_td', 'rec', 'rec_yds', 'rec_td']].astype(int)
-	RBS['pts'] = (RBS['ru_yds'] + RBS['rec_yds']) / 10 + RBS['rec'] * .5 + (RBS['ru_td'] + RBS['rec_td']) * 6
+        vals.insert(0, 'Name', name)
+        if len(vals.columns) != 18 or sum(vals.columns == attributes) != 18:
+            null = pd.DataFrame(columns=attributes)
+            return null
+        else:
+            return vals
 
-	RBS = RBS[RBS.date >= target_date]
-	RBS['date'] = RBS['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    attributes = [
+        'Name', 'Rk', 'G#', 'Date', 'Age', 'Tm', 'Home', 'Opp', 'Result',
+        'Att', 'Yds', 'Y/A', 'TD', 'Tgt', 'Rec', 'Yds', 'Y/R', 'TD'
+    ]
+    RBS = pd.DataFrame(columns=attributes)
 
-	f = open('secret.txt', 'r')
-	secret = f.read()
+    for key in rb_dict:
+        print key
+        p = PlayerScrape(key, rb_dict[key], attributes)
+        if sum(p.columns == RBS.columns) == 18:
+            RBS = RBS.append(p)
+            print key
 
-	connect_string = 'mysql+pymysql://root:%s@127.0.0.1/nfl?charset=utf8mb4' % (secret)
-	engine = sqlalchemy.create_engine(connect_string, echo = False)
-	RBS.to_sql(con = engine, name = 'rb', if_exists = 'append', index = False)
+    target_date = pd.to_datetime(target_date)
+    RBS = RBS.fillna(0)
+    RBS['Home'] = RBS['Home'].replace('@', 0).replace('', 1)
+    RBS['Date'] = RBS['Date'].apply(pd.to_datetime)
+    RBS = RBS.drop(['Rk', 'Age', 'Result', 'Y/A', 'Y/R'], axis=1)
+    RBS = RBS.replace('', 0)
+    RBS.columns = [
+        'name', 'g', 'date', 'team', 'home', 'opp', 'att',
+        'ru_yds', 'ru_td', 'tgt', 'rec', 'rec_yds', 'rec_td'
+    ]
 
+    RBS[['ru_yds', 'ru_td', 'rec', 'rec_yds', 'rec_td']] = RBS[
+        [
+            'ru_yds', 'ru_td', 'rec', 'rec_yds', 'rec_td'
+        ]
+    ].astype(int)
+    RBS['pts'] = (RBS['ru_yds'] + RBS['rec_yds']) / (
+        10 + RBS['rec'] * .5 + (RBS['ru_td'] + RBS['rec_td']) * 6)
 
+    RBS = RBS[RBS.date >= target_date]
+    RBS['date'] = RBS['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+
+    f = open('secret.txt', 'r')
+    secret = f.read()
+
+    connect_string = 'mysql+pymysql://root:%s@127.0.0.1/nfl?charset=utf8mb4'
+    connect_string = connect_string % (secret)
+    engine = sqlalchemy.create_engine(connect_string, echo=False)
+    RBS.to_sql(con=engine, name='rb', if_exists='append', index=False)
