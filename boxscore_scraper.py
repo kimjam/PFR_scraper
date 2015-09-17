9e937fec296de203fcc388e14cd695cd5bfb3e33
kimjam <kimjam@users.noreply.github.com>
2015-09-16 21:16:56 -0400

    fixed qb_scraper

--- boxscore_scraper.py
+++ boxscore_scraper.py
@@ -1,118 +1,107 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import numpy as np
 import pandas as pd
 import itertools
 
+
 def boxscore_scraper(url, target_week, year):
-	def playerscraper(url, year):
-		r = requests.get(url)
-		soup = BeautifulSoup(r.content)
-		
-		table = soup.find('table', {'id': 'skill_stats'})
-		players = table.find_all('td', {'align': 'left', 'style': 'border: 1px dotted #AAA !important;'})
-		
-		names = [p.text for p in players]
-		
-		regex = r'<a href="(.*)">.*<\/a>'
-		links = [re.compile(regex).findall(str(p)) for p in players]
-		chain = itertools.chain.from_iterable(links)
-		links = list(chain)
-		u = 'http://www.pro-football-reference.com'
-		g = 'gamelog/'
-		
-		links = [u + l.replace('.htm', '/') + g + year for l in links]
-		
-		playerlinks = dict(zip(names, links))
-		return(playerlinks)
+    def playerscraper(url, year):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content)
 
-	def positionscraper(url):
-		r = requests.get(url)
-		soup = BeautifulSoup(r.content)
-		
-		regex = r'<strong>Position:</strong>(.*)'
-		pos = [re.compile(regex).findall(str(s)) for s in soup]
-		
-		chain = itertools.chain.from_iterable(pos)
-		pos = list(chain)
-		pos = filter(None, pos)
-		if pos:
-			pos = pos[0].replace(' ', '')
-		else:
-			pos = 'FIND ME BRUH'
-		
-		return(pos)
+        table = soup.find('table', {'id': 'skill_stats'})
+        players = table.find_all(
+            'td',
+            {'align': 'left', 'style': 'border: 1px dotted #AAA !important;'}
+        )
 
-	# url = 'http://www.pro-football-reference.com/years/2014/games.htm'
-	r = requests.get(url)
-	soup = BeautifulSoup(r.content)
+        names = [p.text for p in players]
 
-	tds = soup.find_all('td', {'align': 'center'})
-	regex = r'<a href="(.*)">boxscore</a>'
+        regex = r'<a href="(.*)">.*<\/a>'
+        links = [re.compile(regex).findall(str(p)) for p in players]
+        chain = itertools.chain.from_iterable(links)
+        links = list(chain)
+        u = 'http://www.pro-football-reference.com'
+        g = 'gamelog/'
 
-	links = [re.compile(regex).findall(str(td)) for td in tds]
-	chain = itertools.chain.from_iterable(links)
-	links = list(chain)
+        links = [u + l.replace('.htm', '/') + g + year for l in links]
 
-	u = 'http://www.pro-football-reference.com'
+        playerlinks = dict(zip(names, links))
+        return(playerlinks)
 
-	boxscorelinks = [u + l for l in links]
+    def positionscraper(url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content)
 
-	tds = soup.find_all('td', {'align': 'right'})
-	regex = r'<td align="right" csk=".*">(.*)</td>'
+        regex = r'<strong>Position:</strong>(.*)'
+        pos = [re.compile(regex).findall(str(s)) for s in soup]
 
-	weeks = [re.compile(regex).findall(str(td)) for td in tds]
-	weeks = filter(None, weeks)
-	chain = itertools.chain.from_iterable(weeks)
-	weeks = list(chain)
-	weeks = [week for week in weeks if week != '']
+        chain = itertools.chain.from_iterable(pos)
+        pos = list(chain)
+        pos = filter(None, pos)
+        if pos:
+            pos = pos[0].replace(' ', '')
+        else:
+            pos = 'FIND ME BRUH'
 
-	boxscore = pd.DataFrame(zip(weeks, boxscorelinks), columns = ['week', 'boxscorelink'])
-	boxscore = boxscore[boxscore.week == str(target_week)]
+        return(pos)
 
-	qbs = {}
-	rbs = {}
-	wrs = {}
-	tes = {}
-	find = {}
-	for link in boxscore['boxscorelink']:
-		playerlinks = playerscraper(link, str(year))
-		for key in playerlinks:
-			pos = positionscraper(playerlinks[key])
-			if pos == 'QB':
-				print(key)
-				qbs[key] = playerlinks[key]
-			elif pos == 'RB' or pos == 'FB':
-				print(key)
-				rbs[key] = playerlinks[key]
-			elif pos == 'WR':
-				print(key)
-				wrs[key] = playerlinks[key]
-			elif pos == 'TE':
-				print(key)
-				tes[key] = playerlinks[key]
-			elif pos == 'P' or pos == 'K':
-				continue
-			else:
-				print(key)
-				find[key] = playerlinks[key]
+    # url = 'http://www.pro-football-reference.com/years/2014/games.htm'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content)
 
-	return {'qbs': qbs, 'rbs': rbs, 'wrs': wrs, 'tes': tes, 'find': find }
+    tds = soup.find_all('td', {'align': 'center'})
+    regex = r'<a href="(.*)">boxscore</a>'
 
-def main(url, target_week, year, target_date):
-	link_dict = boxscore_scraper(url, target_week, year)
+    links = [re.compile(regex).findall(str(td)) for td in tds]
+    chain = itertools.chain.from_iterable(links)
+    links = list(chain)
 
-	import qb_scraper
-	import rb_scraper
-	import wr_scraper
-	import te_scraper
+    u = 'http://www.pro-football-reference.com'
 
-	qb_scraper.qb_scraper(link_dict['qbs'], target_date)
-	rb_scraper.rb_scraper(link_dict['rbs'], target_date)
-	wr_scraper.wr_scraper(link_dict['wrs'], target_date)
-	te_scraper.te_scraper(link_dict['tes'], target_date)
+    boxscorelinks = [u + l for l in links]
 
-if __name__ == '__main__':
-	import sys
-	main(url = sys.argv[1], target_week = sys.argv[2], year = sys.argv[3], target_date = sys.argv[4])
+    tds = soup.find_all('td', {'align': 'right'})
+    regex = r'<td align="right" csk=".*">(.*)</td>'
+
+    weeks = [re.compile(regex).findall(str(td)) for td in tds]
+    weeks = filter(None, weeks)
+    chain = itertools.chain.from_iterable(weeks)
+    weeks = list(chain)
+    weeks = [week for week in weeks if week != '']
+
+    boxscore = pd.DataFrame(
+        zip(weeks, boxscorelinks),
+        columns=['week', 'boxscorelink']
+    )
+    boxscore = boxscore[boxscore.week == str(target_week)]
+
+    qbs = {}
+    rbs = {}
+    wrs = {}
+    tes = {}
+    find = {}
+    for link in boxscore['boxscorelink']:
+        playerlinks = playerscraper(link, str(year))
+        for key in playerlinks:
+            pos = positionscraper(playerlinks[key])
+            if pos == 'QB':
+                print(key)
+                qbs[key] = playerlinks[key]
+            elif pos == 'RB' or pos == 'FB':
+                print(key)
+                rbs[key] = playerlinks[key]
+            elif pos == 'WR':
+                print(key)
+                wrs[key] = playerlinks[key]
+            elif pos == 'TE':
+                print(key)
+                tes[key] = playerlinks[key]
+            elif pos == 'P' or pos == 'K':
+                continue
+            else:
+                print(key)
+                find[key] = playerlinks[key]
+
+    return {'qbs': qbs, 'rbs': rbs, 'wrs': wrs, 'tes': tes, 'find': find}
--- qb_scraper.py
+++ qb_scraper.py
@@ -8,7 +8,7 @@ import sqlalchemy
 
 
 def qb_scraper(qb_dict, target_date):
-    def PlayerScrape(name, playerlink):
+    def PlayerScrape(name, playerlink, final_headers):
         url = playerlink
         r = requests.get(url)
         soup = BeautifulSoup(r.content)
@@ -22,6 +22,7 @@ def qb_scraper(qb_dict, target_date):
 
         header = soup.find_all("tr", {"class": ''})[1]
         regex = r'<th .*>(.*)<\/th>'
+
         clmheaders = [re.compile(regex).findall(str(h)) for h in header]
         chain = itertools.chain.from_iterable(clmheaders)
         clmheaders = list(chain)
@@ -30,27 +31,49 @@ def qb_scraper(qb_dict, target_date):
         cols = len(clmheaders)
         rows = len(data)/cols
 
-        vals = pd.DataFrame(index=[name]*(rows-1), columns=clmheaders[0:22])
+        vals = pd.DataFrame(index=[name], columns=clmheaders[0:22])
         breaks = np.linspace(0, len(data), (rows+1), dtype="int16")
 
-        for i in range(rows-1):
-            vals.ix[i] = [d for d in data[breaks[i]:breaks[i+1]]][0:22]
+        if rows == 1:
+            for i in range(1):
+                vals.ix[i] = [d for d in data[breaks[i]:breaks[i+1]]][0:22]
+        else:
+            for i in range(rows-1):
+                vals.ix[i] = [d for d in data[breaks[i]:breaks[i+1]]][0:22]
 
         vals.index.name = 'Name'
         vals.reset_index(level=0, inplace=True)
+        if vals.columns[9] != 'Cmp' and vals.columns[10] != 'Cmp':
+            return vals
+        else:
+            place = vals.shape[1]
+            vals.columns = final_headers[0:place]
+
+            start = vals.shape[1]
+            end = len(final_headers)
+            if vals.shape[1] < 23 and ('Cmp' in clmheaders[8:10]):
+                for col in final_headers[start:end]:
+                    vals[col] = pd.Series([0])
+
+        vals = vals[final_headers]
         return vals
 
     clmheaders = [
         'Name', 'Rk', 'G#', 'Date', 'Age', 'Tm', 'Home', 'Opp',
         'Result', 'GS', 'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'Int', 'Rate',
-        'Y/A', 'AY/A', 'Att', 'Yds', 'Y/A', 'TD', 'Tgt', 'Rec', 'Yds', 'Y/R',
-        'TD'
+        'Y/A', 'AY/A', 'ru_Att', 'ru_Yds', 'ru_Y/A', 'ru_TD', 'Tgt', 'Rec',
+        'rec_Yds', 'rec_Y/R', 'rec_TD'
     ]
 
+    final = clmheaders[0:23]
     QBS = pd.DataFrame(columns=clmheaders[0:23])
     for key in qb_dict:
         print key
-        p = PlayerScrape(key, qb_dict[key])
+        p = PlayerScrape(
+            name=key,
+            playerlink=qb_dict[key],
+            final_headers=final
+        )
         if p.shape[1] == QBS.shape[1]:
             QBS = QBS.append(p)
         else:
@@ -64,7 +87,7 @@ def qb_scraper(qb_dict, target_date):
     QBS['Date'] = QBS['Date'].apply(pd.to_datetime)
     QBS = QBS.replace('', 0)
     QBS = QBS.drop(
-        ['Rk', 'Age', 'Result', 'Cmp%', 'Rate', 'Y/A', 'AY/A'],
+        ['Rk', 'Age', 'Result', 'Cmp%', 'Rate', 'Y/A', 'AY/A', 'ru_Y/A'],
         axis=1
     )
     QBS.columns = [
@@ -76,8 +99,10 @@ def qb_scraper(qb_dict, target_date):
     ] = QBS[
         ['cmp', 'att', 'yds', 'td', 'ints', 'ru_att', 'ru_yds', 'ru_td']
     ].astype(int)
-    QBS['pts'] = QBS['yds'] / 25 + QBS['ru_yds'] / (
-        10 + QBS['td'] * 4 - QBS['ints'] + QBS['ru_td'] * 6)
+    QBS['pts'] = (
+        QBS['yds'] / 25. + QBS['ru_yds'] / 10. +
+        QBS['td'] * 4. - QBS['ints'] + QBS['ru_td'] * 6.
+    )
 
     QBS['pts'] = [round(x, 1) for x in QBS['pts']]
 
